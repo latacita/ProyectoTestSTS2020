e75c47e46d971e9c0ddfa240f7f114a879c78117
Pablo Sánchez <p.sanchez@unican.es>
2020-03-03 16:11:37 +0100

    Create Project

---
+++ src/main/java/es/unican/sanchezbp/ProyectoTest2020Application.java
@@ -0,0 +1,13 @@
+package es.unican.sanchezbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTest2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTest2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/unican/sanchezbp/ProyectoTest2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unican.sanchezbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTest2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
